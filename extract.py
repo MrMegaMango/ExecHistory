8aaa373fed6dc87ad7b32ea1b872eef22c4d0cb9
Zuo Wang <wangzuo95911@gmail.com>
2023-11-29 13:07:01 +0800

    add shadcn

--- extract.py
+++ extract.py
@@ -1,89 +1,54 @@
 import os
-import re
 import json
 
 execution_path = 's3data/executions2/executions2'
-
 app_path = 's3data/app2.json'
+
 with open(app_path) as f:
     app_json = json.load(f)
-
 
 def unroll(output):
     if not isinstance(output, list):
         return list(output.items())
     return [(k, v) for o in output for k, v in o.items()]
 
-
 def get_values_at_end_of_execution(execution_dir):
-    # List all files and sort them
     all_files = sorted([file for file in os.listdir(execution_dir) if ".json" in file])
-
     all_inputs = {}
     for file in all_files:
         with open(os.path.join(execution_dir, file), "r") as f:
             data = json.load(f)
             for name, val in unroll(data):
                 all_inputs[name] = val
-
     return all_inputs
 
-
 def get_input_and_outputs(execution_folder, app_json):
-    # there's one file that finishes with `inputs.json`
     filenames = os.listdir(execution_folder)
     input_filename = [filename for filename in filenames if filename.endswith('inputs.json')][0]
     with open(os.path.join(execution_folder, input_filename)) as f:
         inputs = json.load(f)
 
     outputs = get_values_at_end_of_execution(execution_folder)
-
     return inputs, outputs
 
-result = ""
-
+executions = []
 for folder in os.listdir(execution_path):
     inputs, outputs = get_input_and_outputs(execution_path+"/"+folder, app_json)
-    result += "{EXECUTION ID}"
-    result += "<br>input: "+str(inputs)+"<br> ---- <br> output: "+ str(outputs) + "<br><br>"
+    execution_data = {
+        "id": folder,  # Assuming folder name is the execution ID
+        "input": inputs,
+        "output": outputs
+    }
+    executions.append(execution_data)
 
+from flask import Flask, jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
 
-# inputs, outputs = get_input_and_outputs(execution_path, app_json)
-# result = "input: "+str(inputs)+"<br> ---- <br> output: "+ str(outputs)
-# print(inputs)
-# print('----')
-# print(outputs)
+@app.route('/api/executions')
+def show_history():
+    return jsonify(executions)
 
-html_content = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>app execution history</title>
-</head>
-<body>
-
-<h1>app execution history</h1>
-<p>"""+result+"""</p>
-
-</body>
-</html>
-"""
-
-# Writing the HTML contents to a file
-with open('index.html', 'w') as file:
-    file.write(html_content)
-
-print("HTML file created successfully.")
-
-
-
-
-# from flask import Flask
-# app = Flask(__name__)
-
-# @app.route('/') 
-# def showHistory(): 
-#     return result
-
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run()
---
+++ shadcn-app/src/app/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+interface Execution {
+  id: string;
+  input: { [key: string]: any };
+  output: { [key: string]: any };
+}
+export default function ExecutionHistory() {
+  const [executions, setExecutions] = useState<Execution[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/executions')
+      .then(response => response.json())
+      .then(data => setExecutions(data));
+  }, []);
+
+  const renderObjectAsList = (obj: { [key: string]: any }) => (
+    <ul>
+      {Object.entries(obj).map(([key, value]) => (
+        <li key={key}>{key}: {value.toString()}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      <h1 className='text-3xl'>Execution History</h1>
+      <Accordion type="single" collapsible>
+        {executions.map(execution => (
+          <AccordionItem key={execution.id} value={execution.id}>
+            <AccordionTrigger>Execution ID: {execution.id}</AccordionTrigger>
+            <AccordionContent>
+              <h3>Input</h3>
+              {renderObjectAsList(execution.input)}
+              <h3>Output</h3>
+              {renderObjectAsList(execution.output)}
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
+  );
+}
